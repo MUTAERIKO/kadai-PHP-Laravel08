1a25111fe0ee266abbba637b7a559a7b99e68b8f
MUTAERIKO <eriko_0325@hotmail.co.jp>
2020-09-26 07:43:41 +0000

    first-kadai09

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,25 @@
-
 <?php
-    Route::get('XXX', 'AAAController@bbb');
-?>
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create', 'Admin\ProfileController@add');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+});
+
+
+
 
