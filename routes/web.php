900fc94d5b8fd8f752478116679df7576372925a
MUTAERIKO <eriko_0325@hotmail.co.jp>
2020-09-28 14:21:44 +0000

    first-kadai12

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,18 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+});
+
+
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+});
+
 
 
 
